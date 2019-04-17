77a17aad08c5351cf40d3e1912cd2f5676a0d628
Alan Holt <aholt@proteus.co>
2019-04-17 01:08:40 -0500

    additional code simplification

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,17 +1,16 @@
 import scala.math.{abs, max}
 
 object Main extends App {
-  def doQueens(
-      board: Array[Array[Int]]
-  ) {
-    val queenCount = board.length
-    var slns = 0
+  val n = 8
+  val board = Array.ofDim[Int](n, n)
+  val queenCount = board.length
+  var slns = 0
 
-    case class Point(x: Int, y: Int)
+  case class Point(x: Int, y: Int)
 
-    def threatens(p1: Point, p2: Point) =
-      p1.x == p2.x || 
-      p1.y == p2.y || 
+  def threatens(p1: Point, p2: Point) =
+    p1.x == p2.x ||
+      p1.y == p2.y ||
       (for {
         i <- 0 to max(abs(p1.x - p2.x), abs(p1.y - p2.y))
         if ((p1.x + i == p2.x && p1.y + i == p2.y) ||
@@ -20,38 +19,34 @@ object Main extends App {
           (p1.x + i == p2.x && p1.y - i == p2.y))
       } yield Point(p1.x + i, p1.y + i)).nonEmpty
 
-    def getAllocatedPoints(): Seq[Point] =
-      for {
-        x <- 0 to board.length - 1
-        y <- 0 to board.length - 1
-        if (board(x)(y) != 0)
-      } yield Point(x, y)
+  def getAllocatedPoints(): Seq[Point] =
+    for {
+      x <- 0 to board.length - 1
+      y <- 0 to board.length - 1
+      if (board(x)(y) != 0)
+    } yield Point(x, y)
 
-    def populateBoard(x: Int = 0): Boolean =
-      if (x == queenCount) {
-        val boardStr = board.deep.mkString("\n")
-        println(s"Solution #${slns + 1}: \n----\n$boardStr\n----\n")
-        slns += 1
-        true
-      } else {
-        var result = false
-        for (y <- 0 to queenCount - 1) {
-          val point = Point(x, y)
-          if ((for (p2 <- getAllocatedPoints() if threatens(point, p2)) yield p2).isEmpty) {
-            board(x)(y) = 1
-            result = populateBoard(x + 1) || result
-            board(x)(y) = 0
-          }
+  def doQueens(x: Int = 0): Boolean =
+    if (x == queenCount) {
+      val boardStr = board.deep.mkString("\n")
+      println(s"Solution #${slns + 1}: \n----\n$boardStr\n----\n")
+      slns += 1
+      true
+    } else {
+      var result = false
+      for (y <- 0 to queenCount - 1) {
+        val point = Point(x, y)
+        if ((for (p2 <- getAllocatedPoints() if threatens(point, p2))
+              yield p2).isEmpty) {
+          board(x)(y) = 1
+          result = doQueens(x + 1) || result
+          board(x)(y) = 0
         }
-        result
       }
+      result
+    }
 
-    populateBoard()
-  }
+  doQueens()
 
-  val n = 8
-
-  doQueens(Array.ofDim[Int](n, n))
-  println("Queens Completed!")
-  println()
+  println("Queens Completed!\n")
 }
